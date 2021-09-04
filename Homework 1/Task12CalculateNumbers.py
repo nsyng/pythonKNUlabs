eb5b56eed31ae680837ced2dc0c8b25f46badd62
Nazar Syngaivskyi <nazar.syngaivskyi@gmail.com>
2021-09-05 01:54:06 +0300

    hw 1 done

---
+++ Homework 1/Task12CalculateNumbers.py
@@ -0,0 +1,12 @@
+"""
+Скласти програми для обчислення значень виразів
+
+"""
+
+a = float(input('a: '));
+b = float(input('b: '));
+c = float(input('c: '));
+
+result = (1/a + 1/b)/(1/a + 1/c);
+
+print('Result:', result)
---
+++ Homework 1/Task17PrintVars.py
@@ -0,0 +1,14 @@
+a = 1;
+b = 2;
+c = 3;
+d = 4;
+
+temp = a;
+
+a = d;
+d = c;
+c = b;
+b = temp;
+
+
+print(b, c, d, a)
